28e68f05d8357e8a3ad55b0e23f724e6994ab5a8
Christopher <firedoglo1941@gmail.com>
2020-11-23 09:55:06 -0500

    Update Example.js

--- Commands/Example.js
+++ Commands/Example.js
@@ -1,1 +1,11 @@
-
+module.exports = {
+	commandname: 'ExampleCommand',
+  commandaliases: ['EC', 'EC1'],
+	commanddescription: 'Example command for Restro.',
+  example: "",
+  guildonly: false,
+  delay: false,
+	execute(User, Message, Arguments) {
+    
+  }
+};
