5ef15bd98b88fc380e40bc0ef2d1b8bd6f6d227a
Christopher <firedoglo1941@gmail.com>
2020-11-23 09:49:41 -0500

    Update Example.js

--- Functions/Example.js
+++ Functions/Example.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  ExampleFunction = Class.extend({
+      Execute: function(Args1, Args2) {
+        console.log(`${Args1} ${Args2} are a value of the users choice!`)
+      }
+  })
+}
