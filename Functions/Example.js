0b12ebf23645e80ac9bfb548af0f38ab1029667d
Christopher <firedoglo1941@gmail.com>
2020-12-08 05:24:09 -0500

    Update Example.js

--- Functions/Example.js
+++ Functions/Example.js
@@ -1,7 +1,12 @@
-module.exports = {
-  ExampleFunction: Class.extend({
-      Execute: function(Args1, Args2) {
-        console.log(`${Args1} ${Args2} are a value of the users choice!`)
-      }
-  })
+class Example {
+  FunctionOfStuff(Arguments1, Arguments2) {
+    return console.log(`${Arguments1} and ${Arguments2} have both been identified as arguments.`)
+  }
+  FunctionOfStuff2(Arguments1, Arguments2) {
+    if (isNaN(Arguments1) && isNaN(Arguments2)) {
+      return console.log(Arguments1 + Arguments2)
+    }
+  }
 }
+
+module.exports = Example
