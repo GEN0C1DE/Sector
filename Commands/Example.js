92a433535bb28433f6346221e71a9ad956ffd3dd
Christopher <firedoglo1941@gmail.com>
2020-12-08 07:28:24 -0500

    Update Example.js

--- Commands/Example.js
+++ Commands/Example.js
@@ -8,6 +8,6 @@ module.exports = {
 	execute: function(User, Message, Arguments) {
     		FunctionDirectory = require("/root/Restro/Functions/Example.js")
 		var ExecuteAuthority = new FunctionDirectory()
-		ExecuteAuthority.FunctionOfStuff("This is developed by INE Developers.", "2020 - 2021.")
+		return ExecuteAuthority.FunctionOfStuff("This is developed by INE Developers.", "2020 - 2021.");
 	}
 };
